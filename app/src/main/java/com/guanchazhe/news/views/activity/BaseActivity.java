d46049e5ce0d66d6e89a8846deb3d6b73e03eff9
Ran Zhang <ranzhang.su@gmail.com>
2016-01-30 00:29:17 +0800

    share

--- app/src/main/java/com/guanchazhe/news/views/activity/BaseActivity.java
+++ app/src/main/java/com/guanchazhe/news/views/activity/BaseActivity.java
@@ -1,8 +1,15 @@
 package com.guanchazhe.news.views.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.CallSuper;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import com.guanchazhe.news.R;
 
 /**
  * Created by ranzh on 1/28/2016.
@@ -16,6 +23,33 @@ public abstract class BaseActivity extends AppCompatActivity {
         initUI();
         initToolbar();
     }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.action_share:
+                Intent shareIntent = new Intent();
+                shareIntent.setAction(Intent.ACTION_SEND);
+                shareIntent.putExtra(Intent.EXTRA_TEXT, getResources().getString(R.string.action_share));
+                shareIntent.setType("text/plain");
+
+                startActivity(Intent.createChooser(shareIntent, getResources().getString(R.string.action_share)));
+                return true;
+            default:
+                break;
+        }
+        return false;
+    }
+
 
     protected abstract void initUI();
     protected abstract void  initToolbar();
